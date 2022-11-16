8e8d88101c1e6713d88734af8d34c5d68545f067
Johnson <Johnson>
2022-11-16 21:02:05 +0800

    Fix executing claim repeatedly issue

--- main.go
+++ main.go
@@ -15,11 +15,11 @@ func main() {
 	for {
 		time.Sleep(time.Minute * 5)
 		if time.Now().Hour() == 19 {
-			for _, player := range config.Players {
+			for i, player := range config.Players {
 				if player.Skip || player.LastClaimedDate == time.Now().Format(dateLayout) {
 					continue
 				}
-
+				config.Players[i].LastClaimedDate = time.Now().Format(dateLayout)
 				log.Println(strings.Repeat("*", 100))
 				log.Println(player.Nickname, player.Email)
 				var content string
